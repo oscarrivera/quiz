87a085a16dffdbb09ef1f90e94aba59abcaf0382
oscarrivera <oriveraruiz@gmail.com>
2015-07-04 11:01:56 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var quizController = require ('../controllers/quiz_controller')
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+//Autoload comandos con quizId
+router.param('quizId',quizController.load);
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
