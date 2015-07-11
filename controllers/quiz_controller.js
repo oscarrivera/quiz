e795d43bcb60399a9258776fc31c00bfc4f142ea
oscarrivera <oriveraruiz@gmail.com>
2015-07-11 12:05:09 +0200

    Validación y Iphone5.css

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,11 +19,11 @@ exports.index = function(req, res) {
       quizes.sort(function(a,b){
         return (b.pregunta < a.pregunta)
       })
-      res.render('quizes/index.ejs', { quizes: quizes});
+      res.render('quizes/index.ejs', { quizes: quizes,errors: []});
     }).catch(function(error){next(error);})
   }else{
     models.Quiz.findAll().then(function(quizes) {
-      res.render('quizes/index.ejs', { quizes: quizes});
+      res.render('quizes/index.ejs', { quizes: quizes,errors: []});
     }).catch(function(error){next(error);})
   }
 
@@ -31,7 +31,7 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-    res.render('quizes/show', { quiz: req.quiz});
+    res.render('quizes/show', { quiz: req.quiz,errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -40,7 +40,7 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
       resultado='Correcto';
   }
-  res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado,errors: []});
 };
 // GET /quizes/new
 exports.new = function(req, res) {
@@ -48,15 +48,22 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz,errors: []});
 };
 
 // POST /quizes/create
-exports.create = function(req, res) {
-  var quiz = models.Quiz.build( req.body.quiz );
+exports.create = function(req, res){
+var quiz = models.Quiz.build( req.body.quiz );
 
-// guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })   // res.redirect: Redirección HTTP a lista de preguntas
+var errors = quiz.validate();//ya qe el objeto errors no tiene then(
+if (errors)
+{
+var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+for (var prop in errors) errores[i++]={message: errors[prop]};
+res.render('quizes/new', {quiz: quiz, errors: errores});
+} else {
+quiz // save: guarda en DB campos pregunta y respuesta de quiz
+.save({fields: ["pregunta", "respuesta"]})
+.then( function(){ res.redirect('/quizes')}) ;
+}
 };
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var quizController = require ('../controllers/quiz_controller')
 //Pagina de Entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz',errors: [] });
 });
 //Autoload comandos con quizId
 router.param('quizId',quizController.load);
@@ -12,7 +12,7 @@ router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author',function (req,res){
-  res.render('author',{autor:'Oscar Rivera'})
+  res.render('author',{autor:'Oscar Rivera',errors: []})
 });
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
