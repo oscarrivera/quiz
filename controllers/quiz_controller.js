e61f4b26c56068df4c27b4a9d52ea73e67517375
oscarrivera <oriveraruiz@gmail.com>
2015-07-17 18:53:05 +0200

    Actualizado sequelize, modificado controlador y pruebas en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -55,47 +55,49 @@ exports.new = function(req, res) {
 };
 
 // POST /quizes/create
-exports.create = function(req, res){
-var quiz = models.Quiz.build( req.body.quiz );
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
 
-var errors = quiz.validate();//ya qe el objeto errors no tiene then(
-if (errors)
-{
-var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
-for (var prop in errors) errores[i++]={message: errors[prop]};
-res.render('quizes/new', {quiz: quiz, errors: errores});
-} else {
-quiz // save: guarda en DB campos pregunta y respuesta de quiz
-.save({fields: ["pregunta", "respuesta", "tema"]})
-.then( function(){ res.redirect('/quizes')}) ;
-}
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta","tema"]})
+        .then( function(){ res.redirect('/quizes')})
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  ).catch(function(error){next(error)});
 };
+
 // GET /quizes/:id/edit
 exports.edit = function(req, res) {
   var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
 
   res.render('quizes/edit', {quiz: quiz, errors: []});
 };
+
 // PUT /quizes/:id
 exports.update = function(req, res) {
-
   req.quiz.pregunta  = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
   req.quiz.tema = req.body.quiz.tema;
-  var quiz = models.Quiz.build( req.body.quiz );
-  var errors = quiz.validate();//ya qe el objeto errors no tiene then(
-
-      if (errors) {
-        var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
-        for (var prop in errors) errores[i++]={message: errors[prop]};
-        res.render('quizes/edit', {quiz: req.quiz, errors: errores});
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
       } else {
         req.quiz     // save: guarda campos pregunta y respuesta en DB
-        .save( {fields: ["pregunta", "respuesta", "tema"]})
+        .save( {fields: ["pregunta", "respuesta","tema"]})
         .then( function(){ res.redirect('/quizes');});
       }     // Redirección HTTP a lista de preguntas (URL relativo)
-
-
+    }
+  ).catch(function(error){next(error)});
 };
 
 // DELETE /quizes/:id
