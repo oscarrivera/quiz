4a008ddb9e6ea91ab818d651d69130dce46e9988
oscarrivera <oriveraruiz@gmail.com>
2015-07-24 20:03:12 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', function(req, res) {
 });
 //Autoload comandos con quizId
 router.param('quizId',quizController.load);
+router.param('commentId', commentController.load);  // autoload :commentId
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
 router.post('/login', sessionController.create);  // crear sesión
@@ -27,6 +28,9 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+	                                    sessionController.loginRequired, commentController.publish);
+
 
 
 module.exports = router;
